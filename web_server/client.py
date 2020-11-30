8c4e129e8eaefadbdc637d772c3634a115d0eb45
Oğuzhan Agkuş <agkusoguzhan@gmail.com>
2020-11-30 20:37:07 +0300

    Web server completed

---
+++ web_server/client.py
@@ -0,0 +1,60 @@
+"""
+This is my simple web client which uses HTTP headers.
+It sends a request for single web page.
+You can pass arguments from command line.
+Pass 3 arguments or none:
+  - first argument: server host
+  - second argument: server port
+  - third argument: filename / page
+
+If you do not pass any arguments, it send request to localhost:80/index.html URL.
+"""
+
+import socket, sys
+
+# Default arguments
+server_host = "localhost"
+server_port = 80
+filename = "index.html"
+
+# If any argument passed
+if len(sys.argv) == 4:
+  server_host = sys.argv[1]
+  server_port = int(sys.argv[2])
+  filename = sys.argv[3]
+
+# Global variables
+server_address = (server_host, server_port)
+connection = None
+
+# Prepare socket
+try:
+  connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  connection.connect(server_address)
+except Exception as error:
+  print("Error occured!", error)
+  sys.exit(1)
+
+# Prepare headers
+request_line = "GET /" + filename + " HTTP/1.1\r\n" 
+header_lines = "Host: " + server_host + "\r\n"
+header_lines += "Connection: close\r\n"
+blank_line = "\r\n"
+request_message = request_line + header_lines + blank_line
+
+# Send headers
+connection.send(request_message.encode())
+
+# Recieve data
+response = ""
+while True:
+    data = connection.recv(1024).decode()
+    if not data:
+        break
+    response += data 
+
+# Print data
+print(response)
+
+# Close socket
+connection.close()
---
+++ web_server/server.py
@@ -0,0 +1,95 @@
+"""
+This is my simple web server which uses HTTP headers.
+It accepts only GET method and return the requested html page.
+Since it is a simple server, I have only index.html page.
+If you request another page, you will get 404 (Not found).
+If your request include any method rather than GET, you will get 405 (Method not allowed!).
+
+TODO:
+  - Create a thread for each request.
+  - Send favicon.ico
+
+NOTE:
+  - Chrome's mobile application acts a little bit weird.
+"""
+
+import socket, signal, datetime
+
+# Set global variables
+server_address = ("", 80)
+server_socket = None
+
+# If CTRL+C occurs
+def signal_handler(signal, frame):
+  server_socket.close()
+  exit(0)
+
+# Prepare socket
+try:
+  server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  server_socket.bind(server_address)
+  server_socket.listen(5)
+except Exception as error:
+  print("Error occured!", error)
+  exit(1)
+
+# Set signal handler
+signal.signal(signal.SIGINT, signal_handler)
+
+# Main loop
+while True:
+  # Accept a connection
+  print("Server is ready!")
+  client_socket, address = server_socket.accept()
+  client_socket.settimeout(5)
+
+  try:
+    request = client_socket.recv(1024).decode()
+    items = request.split()
+    
+    request_type = items[0]
+    if request_type != "GET":
+      raise Exception("Method not allowed!")
+
+    filename = items[1]
+    file = open(filename[1:])
+    data = file.read()
+    file.close()
+
+    # Prepare headers
+    status_line = "HTTP/1.1 200 OK\r\n"
+    header_lines = "Connection: keep-alive\r\n"
+    header_lines += "Date: {}\r\n".format(datetime.datetime.now())
+    header_lines += "Server: MyServer\r\n"
+    header_lines += "Content-Length: {}\r\n".format(len(data))
+    header_lines += "Content-Type: text/html\r\n"
+    blank_line = "\r\n"
+    response_message = status_line + header_lines + blank_line
+
+    # Send headers
+    client_socket.send(response_message.encode())
+  
+    # Send content of the file
+    for i in range(len(data)):
+      client_socket.send(data[i].encode())
+    
+    print(filename + " sent to " + address[0])
+
+    # Close client socket
+    client_socket.shutdown(socket.SHUT_RDWR)
+    client_socket.close()
+ 
+  except Exception as error:
+    # Print and response error message
+    print("Error occured!", error)
+    if (error == "Method not allowed!"):
+      client_socket.send("HTTP/1.1 405 METHOD NOT ALLOWED\r\n".encode())
+    else:
+      client_socket.send("HTTP/1.1 404 NOT FOUND\r\n".encode())
+
+    # Close client socket  
+    client_socket.shutdown(socket.SHUT_RDWR)
+    client_socket.close()
+  
+server_socket.close()
